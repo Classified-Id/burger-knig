24ff2a038019ae33a892f70fe75bbe19e4401443
Classified <mur.sensi@yandex.ru>
2025-03-23 16:15:33 +0300

    style: переразложил файлы

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './app';
+
+import { App } from '@components/app';
+
 import './styles.css';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 root.render(
-	<StrictMode>
-		<App />
-	</StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
